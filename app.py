856f25c4c82d214567970c9eb4ca3186b4b2972c
Pavel Kopecky <pavelkopecky20@gmail.com>
2025-01-07 13:23:45 +0100

    Vyřešena administrace v app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from extensions import db, mail
-from routes.main_routes import bp as main_routes  # Import blueprintu
+from routes.main_routes import bp as main_routes  # Import blueprintu hlavních stránek
+from routes.admin_routes import bp as admin_routes  # Import blueprintu pro administraci
 
 app = Flask(__name__)
 
@@ -10,8 +11,9 @@ app.config.from_object('config.Config')  # Načte konfiguraci
 db.init_app(app)
 mail.init_app(app)
 
-# Registrace blueprintu
-app.register_blueprint(main_routes)
+# Registrace blueprintů
+app.register_blueprint(main_routes)  # Registrace hlavního blueprintu
+app.register_blueprint(admin_routes, url_prefix='/admin')  # Registrace admin blueprintu
 
 if __name__ == '__main__':
     app.run(debug=True)
